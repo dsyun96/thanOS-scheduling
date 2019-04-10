a89f4da1020ba032208ed55f4051c45b6d36749d
YunGoon <dsyun96@naver.com>
2019-04-10 14:41:38 +0900

    FCFS function

--- js/execute.js
+++ js/execute.js
@@ -1,7 +1,26 @@
-function FCFS(processes) {
-    processes = [["P1", 0, 1], ["P2", 1, 1], ["P3", 0, 2]]; //나중에 삭제 예정
-    let result = [];
+/*
+입력: 프로세스개수 n 도착시간배열 arrivals[2, 5, ...] 수행시간배열 bursts[3, 7, ...]
+출력: 대기시간배열 w[] 반환시간배열 t[] 프로세서상태 p[[processIdx, timeStart, timeStay], ...]
+ */
+
+function FCFS(n, arrivals, bursts) {
+    let processes = [];
+    for (let i = 0; i < n; ++i) processes.push([i, arrivals[i], bursts[i]]);
     processes.sort((a, b) => a[1] - b[1] ? a[1] - b[1] : a[0] - b[0]);
-    processes.forEach((a) => result.push([a[0], a[2]]));
-    return result;
+
+    let waiting = [], turnAround = [], pState = [];
+    let time = 0;
+
+    for (let i = 0; i < n; ++i) {
+        // i번째로 들어온 프로세스 수행
+        let now = processes[i];
+
+        turnAround[now[0]] = time + now[2] - now[1];
+        waiting[now[0]] = turnAround[now[0]] - bursts[now[0]];
+        pState.push([now[0], time, now[2]]);
+
+        time += now[2];
+    }
+
+    return [waiting, turnAround, pState];
 }
