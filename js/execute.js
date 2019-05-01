5a75d7d34dddc312126d36337c4de3429ff11a3e
YunGoon <dsyun96@naver.com>
2019-05-02 00:27:44 +0900

    Update execute.js

--- js/execute.js
+++ js/execute.js
@@ -48,18 +48,19 @@ function SRTN(cntProcess, arrivals, bursts) {
     let waitings = [null], turnArounds = [null], pStates = [];
     let time = 0;
     
+    arrivals.shift();
     arrivals = Array.from(new Set(arrivals));
-    arrivals.pop();
-    arrivals.push(1e200);
+    arrivals.push(Infinity);
+    arrivals.sort();
     
     for (let i = 0; i < arrivals.length - 1; ++i) {
         // eventTime = 이벤트가 발생한 시간
         let eventTime = arrivals[i];
-        let tmpProc = [];
+        let readyQ = [];
         let nextTime = arrivals[i + 1];
         
         for (let j = 0; j < cntProcess; ++j)
-            if (processes[j].arrival <= eventTime) tmpProc.push(j);
+            if (processes[j].arrival <= eventTime && processes[j].burst > 0) readyQ.push(j);
         
         // 지금부터 다음 이벤트가 발생하기 전까지는 burst 가장 짧은 놈을 계속 돌린다
         while (time < nextTime) {
@@ -68,22 +69,38 @@ function SRTN(cntProcess, arrivals, bursts) {
                 time = eventTime;
             }
             
-            let idx = -1, max = 1e200;
-            for (let j = 0; j < tmpProc.length; ++j) {
-                let now = processes[tmpProc[j]];
-                if (now.burst < max) {
+            let idx = -1, max = Infinity;
+            for (let j = 0; j < readyQ.length; ++j) {
+                let now = processes[readyQ[j]];
+                if (now.burst > 0 && now.burst < max) {
                     max = now.burst;
-                    idx = tmpProc[j];
+                    idx = readyQ[j];
                 }
             }
+
+            if (idx === -1) break;
             
             // idx번째가 가장 짧은 놈
             let exec = Math.min(nextTime - time, processes[idx].burst);
-            pStates.push([idx, time, exec]);
+            pStates.push([processes[idx].pid, time, exec]);
             time += exec;
             processes[idx].burst -= exec;
+
+            if (processes[idx].burst === 0) {
+                let pid = processes[idx].pid;
+                turnArounds[pid] = time - processes[idx].arrival;
+                waitings[pid] = turnArounds[pid] - bursts[pid];
+            }
         }
     }
+
+    let tmp = [pStates[0]], p = 0;
+    for (let i = 1; i < pStates.length; ++i) {
+        if (tmp[p][0] === pStates[i][0]) tmp[p][2] += pStates[i][2];
+        else tmp.push(pStates[i]), ++p;
+    }
+
+    pStates = tmp;
     
     return [waitings, turnArounds, pStates];
 }
